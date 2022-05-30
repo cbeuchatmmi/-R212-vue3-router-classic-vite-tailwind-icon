4e89c6ae4a2b1b6db28522dc752efb877c19e6a8
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2022-05-30 22:34:43 +0200

    Festival responsive div

--- src/views/Festival.vue
+++ src/views/Festival.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <div>
-      <img src="src/assets/image/festival.jpg" alt="Image d'une scène avec foule" />
+      <img class="w-full" src="src/assets/image/festival.jpg" alt="Image d'une scène avec foule" />
       <!--<h1 class="text-[81px] text-rose-600"> 
         <span class="h-[638px] w-[375px] text-center">SUN</span><br /> 
         <span class="h-[638px] w-[375px] text-center">&#x26;</span><br /> 
@@ -16,16 +16,18 @@
         tournois de beach volley et de pétanque, différent bar et stand de restorations.
       </p>
     </div>
-    <div>
-      <h2 class="p-6 text-[29px] text-rose-600">Belfort, le Lac du Malsaucy</h2>
-      <img src="../assets/image/carte.jpg" alt="Carte de France" class="w-full" />
-      <p class="left-[19px] top-[1083px] m-8 text-left text-[23px] text-black">
-        Installé sur les rives du Malsaucy, véritable écrin dans une zone naturelle protégée (le Malsaucy et la Véronne). Entre collines et
-        étendues d’eau, à 7 kilomètres de la ville de Belfort, le lac du Malsaucy (propriété du Conseil Départemental 90) accueille une base
-        nautique, une plage, la Maison Départementale de l’environnement ainsi qu’une zone protégée de nidification d’oiseaux certifiée LPO.
-        Un emplacement calme et serain pour notre festival Sun &amp; Chill.
-      </p>
-      <img src="../assets/image/lac.jpg" alt="Photo du Lac du Malsaucy" class="w-full" />
+    <div class="bg-amber-300">
+      <h2 class="p-6 text-center text-[29px] text-rose-600">Belfort, le Lac du Malsaucy</h2>
+      <div class="grid xl:grid-cols-2">
+        <img src="../assets/image/carte.jpg" alt="Carte de France" class="m-auto mb-8 w-full md:w-[535px]" />
+        <p class="left-[19px] top-[1083px] m-8 text-left text-[23px] text-black">
+          Installé sur les rives du Malsaucy, véritable écrin dans une zone naturelle protégée (le Malsaucy et la Véronne). Entre collines
+          et étendues d’eau, à 7 kilomètres de la ville de Belfort, le lac du Malsaucy (propriété du Conseil Départemental 90) accueille une
+          base nautique, une plage, la Maison Départementale de l’environnement ainsi qu’une zone protégée de nidification d’oiseaux
+          certifiée LPO. Un emplacement calme et serain pour notre festival Sun &amp; Chill.
+        </p>
+      </div>
+      <img src="../assets/image/lac.jpg" alt="Photo du Lac du Malsaucy" class="m-auto mb-4 w-full md:w-[763px]" />
     </div>
   </main>
 </template>
