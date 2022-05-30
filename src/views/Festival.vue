051de61e98ff2ca1f24eedfffee5f61b1e5a597a
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2022-05-30 21:01:40 +0200

    Festival

---
+++ src/views/Festival.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <div>
+      <img src="src/assets/image/festival.jpg" alt="Image d'une scène avec foule" />
+      <!--<h1 class="text-[81px] text-rose-600"> 
+        <span class="h-[638px] w-[375px] text-center">SUN</span><br /> 
+        <span class="h-[638px] w-[375px] text-center">&#x26;</span><br /> 
+        <span class="h-[638px] w-[375px] text-center">CHILL</span> 
+      </h1> -->
+    </div>
+    <h2 class="p-6 text-[29px] text-rose-600">Le Festival</h2>
+    <div class="text-left text-[23px] text-black">
+      <p class="left-[19px] top-[1083px] m-4">
+        Le Sun &amp; Chil Festival est un festival de musique chill out où la bonne ambuance est de mise. L’objectif de notre festival est
+        de vous faire passer un bon moment durant l’été en vous proposant durant deux soirée différents évènement comme un concert, des
+        tournois de beach volley et de pétanque, différent bar et stand de restorations.
+      </p>
+    </div>
+    <div>
+      <h2 class="p-6 text-[29px] text-rose-600">Belfort, le Lac du Malsaucy</h2>
+      <img src="../assets/image/carte.jpg" alt="Carte de France" class="w-full" />
+      <p class="left-[19px] top-[1083px] m-8 text-left text-[23px] text-black">
+        Installé sur les rives du Malsaucy, véritable écrin dans une zone naturelle protégée (le Malsaucy et la Véronne). Entre collines et
+        étendues d’eau, à 7 kilomètres de la ville de Belfort, le lac du Malsaucy (propriété du Conseil Départemental 90) accueille une base
+        nautique, une plage, la Maison Départementale de l’environnement ainsi qu’une zone protégée de nidification d’oiseaux certifiée LPO.
+        Un emplacement calme et serain pour notre festival Sun &amp; Chill.
+      </p>
+      <img src="../assets/image/lac.jpg" alt="Photo du Lac du Malsaucy" class="w-full" />
+    </div>
+  </main>
+</template>
+
+<script>
+</script>
+
+
