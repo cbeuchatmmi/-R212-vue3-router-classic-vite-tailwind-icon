5cbd9b106c9c9091f3429441ce2496cd175f76e0
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2022-05-31 21:49:57 +0200

    festival dark

--- src/views/Festival.vue
+++ src/views/Festival.vue
@@ -1,11 +1,11 @@
 <template>
-  <main>
+  <main class="bg-yellow-50 dark:bg-black">
     <div>
       <img class="w-full" src="src/assets/image/festival.jpg" alt="Image d'une scène avec foule" />
     </div>
     <h2 class="p-6 font-PerMarker text-[29px] text-rose-600">Le Festival</h2>
     <div class="text-left text-[23px] text-black">
-      <p class="left-[19px] top-[1083px] m-4 lg:text-[28px]">
+      <p class="left-[19px] top-[1083px] m-4 dark:text-white lg:text-[28px]">
         Le Sun &amp; Chil Festival est un festival de musique chill out où la bonne ambuance est de mise. L’objectif de notre festival est
         de vous faire passer un bon moment durant l’été en vous proposant durant deux soirée différents évènement comme un concert, des
         tournois de beach volley et de pétanque, différent bar et stand de restorations.
@@ -15,7 +15,7 @@
       <h2 class="p-6 text-center font-PerMarker text-[29px] text-rose-600">Belfort, le Lac du Malsaucy</h2>
       <div class="grid xl:grid-cols-2">
         <img src="../assets/image/carte.jpg" alt="Carte de France" class="m-auto mb-8 w-full md:w-[535px]" />
-        <p class="left-[19px] top-[1083px] m-8 text-left text-[23px] text-black lg:text-[28px]">
+        <p class="left-[19px] top-[1083px] m-8 text-left text-[23px] text-black dark:text-white lg:text-[28px]">
           Installé sur les rives du Malsaucy, véritable écrin dans une zone naturelle protégée (le Malsaucy et la Véronne). Entre collines
           et étendues d’eau, à 7 kilomètres de la ville de Belfort, le lac du Malsaucy (propriété du Conseil Départemental 90) accueille une
           base nautique, une plage, la Maison Départementale de l’environnement ainsi qu’une zone protégée de nidification d’oiseaux
