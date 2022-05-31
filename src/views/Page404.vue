ffdd248b95545130dc5c40d6ed0ff64f953e65c6
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2022-05-31 22:24:45 +0200

    intégration p de la page 404

--- src/views/Page404.vue
+++ src/views/Page404.vue
@@ -1,3 +1,7 @@
-<template></template>
+<template>
+  <main class="bg-yellow-50 dark:bg-black">
+    <p class="dark:text-white">La Page que vous cherchez n'existe pas. Merci de retourner sur la page d'accueil.</p>
+  </main>
+</template>
 <script>
 </script>
