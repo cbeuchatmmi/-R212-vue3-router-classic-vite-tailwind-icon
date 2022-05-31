c8676dd80ba1a41a3d6cbce959d3afb212034a80
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2022-05-31 21:29:56 +0200

    Annulée

--- src/components/bg-jaune.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <svg width="500" height="746" viewBox="0 0 375 746" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
-    <path d="M375 58L0 0V689L375 746V58Z" fill="#FCD34D"></path>
-  </svg>
-</template>
