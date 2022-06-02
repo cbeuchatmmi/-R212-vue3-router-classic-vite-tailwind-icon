08682c852530334a9321c9833c39cd7e28a66b61
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2022-06-02 21:27:06 +0200

    Page 404

--- src/views/Page404.vue
+++ src/views/Page404.vue
@@ -1,6 +1,9 @@
 <template>
   <main class="bg-yellow-50 dark:bg-black">
-    <p class="dark:text-white">La Page que vous cherchez n'existe pas. Merci de retourner sur la page d'accueil.</p>
+    <h2 class="p-6 text-center font-PerMarker text-[80px] font-bold text-rose-600 dark:text-white lg:text-[64px]">Erreur 404</h2>
+    <p class="text-center text-[50px] font-bold dark:text-white">
+      La Page que vous cherchez n'existe pas. Merci de retourner sur la page d'accueil.
+    </p>
   </main>
 </template>
 <script>
