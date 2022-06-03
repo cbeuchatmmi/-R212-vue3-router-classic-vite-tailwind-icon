30197a6ef46633f279a7fc8b9d4d982463dc778f
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2022-06-03 16:39:23 +0200

    correction {description}

--- src/components/tournoi.vue
+++ src/components/tournoi.vue
@@ -12,7 +12,7 @@ export default {
     <div class="mb-4 text-center">
       <h2 class="p-6 font-PerMarker text-[29px] text-rose-600 dark:text-white">{{ nom }}</h2>
       <img class="m-auto" :src="image" alt="image du tournoi" />
-      <p class="dark:text-white lg:text-[28px]">{{ descritpion }}</p>
+      <p class="dark:text-white text-black lg:text-[28px]">{{ description }}</p>
     </div>
   </figure>
 </template>
